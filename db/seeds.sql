3b3e498510fa3678f87d9f62f69e097e4ee94449
Paul David Lampe <pdlampe@gmail.com>
2019-09-08 21:42:39 -0500

    Created example database items

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Steakhouse Burger', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Road Hoss Burger', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
