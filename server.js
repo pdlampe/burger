2c8356f1551218e8038f0f219151ea2cee52826d
Paul David Lampe <pdlampe@gmail.com>
2019-09-08 21:23:33 -0500

    Server file created

---
+++ server.js
@@ -0,0 +1,29 @@
+var connection = require("./config/connection");
+var express = require('express');
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/public'));
+app.use('/', require('./controllers/burgerController'));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+connection.connect(err => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(PORT, () => {
+            console.log(`App listening on port ${PORT}`);
+
+        });
+
+    }
+
+});
