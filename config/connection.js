2446c8681c9f8a9290ccf19441cafa8f3ac76be0
Paul David Lampe <pdlampe@gmail.com>
2019-09-11 22:39:46 -0500

    Updated Connections JS

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "SourPablo1985!!",
-    database: "burger_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
@@ -17,5 +20,5 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+
 module.exports = connection;
