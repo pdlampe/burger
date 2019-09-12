7145693d0fa14235bf82bd329fb4033a8e05d86a
Paul David Lampe <pdlampe@gmail.com>
2019-09-11 22:28:34 -0500

    Updated Server JS

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
-var connection = require("./config/connection");
 var express = require('express');
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var methodOverride = require("method-override");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/public'));
-app.use('/', require('./controllers/burgerController'));
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-connection.connect(err => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        app.listen(PORT, () => {
-            console.log(`App listening on port ${PORT}`);
+var routes = require("./controllers/burgers_controller.js");
 
-        });
+app.use("/", routes);
 
-    }
-
-});
+app.listen(PORT);
