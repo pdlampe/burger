e89dad0abac9822e10a1da92bcfc7e19c91d31c2
Paul David Lampe <pdlampe@gmail.com>
2019-09-12 21:26:31 -0500

    Most recent version of burger app

--- config/orm.js
+++ config/orm.js
@@ -6,45 +6,84 @@
 
 ///Export the ORM object in module.exports.
 
-var connection = require("./connection");
+var connection = require("../config/connection.js");
+
+
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function sendObjToSQL(ob) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasUniqueProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0 {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+
+        }
+    }
+    return arr.toString();
+}
+
 
 var orm = {
-    selectAll: function (tableName) {
-        var query = "SELECT * FROM ??";
-        return new Promise((resolve, reject) => {
-            connection.query(query, [tableName], (err, results) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(results);
-                }
-            });
+    selectAll: function (table, cb) {
+        var queryString = "SELECT * FROM ??";
+        console.log(queryString);
+        connection.query(queryString, [table], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
     },
-    insertOne: function (tableName, obj) {
-        var query = `INSERT INTO BURGERS SET ?`;
-        return new Promise((resolve, reject) => {
-            connection.query(query, obj, (err, results) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(results);
-                }
-            });
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
         });
     },
-    updateOne: function (tableName, updCol, updVal, idCol, objId) {
-        var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
-        return new Promise((resolve, reject) => {
-            connection.query(query, [tableName, updCol, updVal, idCol, objId], (err, results) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(results);
-                }
-            });
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
         });
     }
-};
+}
+
 
 module.exports = orm;
